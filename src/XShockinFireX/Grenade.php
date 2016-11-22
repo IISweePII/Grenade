ea0e7ac80e87761c41ea6f2684991ed81423ed07
IISweepeDII <sweeps.gh@gmail.com>
2016-11-22 13:44:12 -0500

    Snowballs are now grenades rather than XP Bottles
    xD

--- src/XShockinFireX/Grenade.php
+++ src/XShockinFireX/Grenade.php
@@ -3,7 +3,7 @@ namespace XShockinFireX;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
-use pocketmine\entity\ThrownExpBottle;
+use pocketmine\entity\Snowball
 use pocketmine\Level;
 use pocketmine\utils\Config;
 use pocketmine\level\Explosion;
@@ -21,7 +21,7 @@ class Grenade extends PluginBase implements Listener {
 	
     public function onProjectileHit(ProjectileHitEvent $event) {
 	$entity = $event->getEntity();
-	if($entity instanceof ThrownExpBottle) {
+	if($entity instanceof Showball) {
 	    $pos = new Position($entity->getX(), $entity->getY(), $entity->getZ(), $entity->getLevel());
 	    $explosion = new Explosion($pos, $this->getConfig()->get("explode-radius"), NULL, $this->getConfig->get("drop-items"));
 	    if($this->getConfig()->get("explode-blocks")) {
